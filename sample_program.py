3c2423e720a2f7e9d13bf474fe3c33504d96873d
Akash <akashironman7@gmail.com>
2016-09-18 18:59:47 +0530

    First Python simple script

---
+++ sample_program.py
@@ -0,0 +1,9 @@
+__author__ = 'akash'
+
+def first_function():
+	input_string = input('please enter string ')
+	print type(input_string)
+	print len(input_string)
+
+if __name__ == "__main__":
+	first_function()
