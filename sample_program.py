4380bdb4a2e2578f46e1f6ff3b9cbac830d1ba63
Akash <akashironman7@gmail.com>
2016-09-19 20:52:07 +0530

    First Executable Program

--- sample_program.py
+++ sample_program.py
@@ -1,9 +1,10 @@
-__author__ = 'akash'
+__author__ ='akash'
 
 def first_function():
-	input_string = input('please enter string ')
+
+	input_string = raw_input('please enter string ')
 	print type(input_string)
 	print len(input_string)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 	first_function()
