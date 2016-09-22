ff38ba9615dc66da167022f81a446389cce086ca
Akash <akashironman7@gmail.com>
2016-09-22 18:46:57 +0530

    A2+B2=C2 condition test code/ if condition test

---
+++ Sample_for_loop.py
@@ -0,0 +1,11 @@
+__author__ ='akash'
+def function():
+	a= input('A=: ')
+	b= input('B=: ')
+	c= input('c=: ')
+	if a**2+b**2 == c**2:
+		print ('A^n +b^n= C^n')
+	else:
+		print('not equal')
+if __name__=='__main__':
+	function()
