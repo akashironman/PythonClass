968e1fcc5b87f000d0394c382c00f8e40450f467
akash <akashironman7@gmail.com>
2016-09-29 19:57:04 +0530

    Factorial program by recursive method + errors resolved

--- fact_by_recursive.py
+++ fact_by_recursive.py
@@ -1,11 +1,14 @@
 __author__='Akash'
+def factorial(n):
+	if n==0:
+		return 1
+	else:
+		return n*factorial(n-1)
+
 def fact():
 	n=input('Enter number to find factorial: ')
-	def factorial(n):
-		if n==0:
-			return 1
-		else:
-			return n*factorial(n-1)
+	fact = factorial(n)
 	print 'Factorial', fact
+
 if __name__=='__main__':
 	fact()
