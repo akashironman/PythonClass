838d75d1e6b8c5a752b190022987850719c219c4
akash <akashironman7@gmail.com>
2016-09-29 16:31:12 +0530

    Factorial program using for loop

---
+++ fact_by_for.py
@@ -0,0 +1,10 @@
+__author__='Akash'
+def function():
+	n=input('Enter no to find factorial: ')
+	fact=1
+	for i in range(5)
+		i+=1
+		fact=fact*i
+	print 'Factorial: ',fact
+if __name__=='__main__':
+	function()
