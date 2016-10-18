661ec2b5ac5a2691d9fc189f014cf18dd457dbc1
Akash <akashironman7@gmail.com>
2016-10-18 15:13:41 +0530

    Exercize Code to check the syntax of braces like {}()[]

---
+++ Parenthesis-check.py
@@ -0,0 +1,21 @@
+__author__='Akash'
+
+def Function():
+	print '----------------------------------------------'
+	print 'Program for [](){} type checking:'
+	print '----------------------------------------------'
+	string=raw_input('Enter the parenthesis string: ')
+	flag =1
+	for i in xrange(0,len(string)-1,2):
+		# print i
+		if (string[i]=='(' and string[i+1]==')') or (string[i]=='[' and string[i+1]==']') or (string[i]=='{' and string[i+1]=='}'):
+			pass
+		else:
+			flag=0
+	if flag==0:
+		print 'invalid string'
+	else:
+		print 'valid string'	
+
+if __name__=='__main__':
+	Function()
