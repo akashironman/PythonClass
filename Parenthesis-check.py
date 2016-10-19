771070307ebaff73a4edaf4636c680bc51a57379
Akash <akashironman7@gmail.com>
2016-10-19 12:30:22 +0530

    Code changed bcz giving error for odd input

--- Parenthesis-check.py
+++ Parenthesis-check.py
@@ -6,6 +6,8 @@ def Function():
 	print '----------------------------------------------'
 	string=raw_input('Enter the parenthesis string: ')
 	flag =1
+	if len(string)%2==1:
+		flag=0
 	for i in xrange(0,len(string)-1,2):
 		# print i
 		if (string[i]=='(' and string[i+1]==')') or (string[i]=='[' and string[i+1]==']') or (string[i]=='{' and string[i+1]=='}'):
