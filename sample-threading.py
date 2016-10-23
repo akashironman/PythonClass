40ff0ef74b758d4b43508f633935e7dac1a6df6c
Akash <akashironman7@gmail.com>
2016-10-23 11:19:38 +0530

    Thread code for simple thread operation

---
+++ sample-threading.py
@@ -0,0 +1,8 @@
+__author__='Akash'
+import threading
+def main():
+    print threading.active_count()
+    print threading.enumerate()
+    print threading.current_thread()
+if __name__=='__main__':
+    main()
